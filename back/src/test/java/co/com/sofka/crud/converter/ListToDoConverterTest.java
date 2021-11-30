9c94bc929733bfc8cfab4dd89ba0c53b25270526
Yenifer <guajeyenifer@gmail.com>
2021-11-29 23:53:03 -0500

    Se realiza la comprobacion con un test de un converter de ListTodo a un DTO ListTodoDTO

--- back/src/test/java/co/com/sofka/crud/converter/ListToDoConverterTest.java
+++ back/src/test/java/co/com/sofka/crud/converter/ListToDoConverterTest.java
@@ -25,4 +25,22 @@ class ListToDoConverterTest {
         Assertions.assertEquals(expected.getClass(), result.getClass());
     }
 
+    @Test
+    public void givenAnListTodoReturnAnListTodoDTO() {
+
+        ListTodo listTodo = new ListTodo();
+        listTodo.setName("entrenar");
+        listTodo.setId(1L);
+
+        ListTodoDTO listTodoDTO = new ListTodoDTO();
+        listTodoDTO.setName("entrenar");
+        listTodoDTO.setId(1L);
+
+        ListToDoConverter listToDoConverter = new ListToDoConverter();
+
+        ListTodoDTO result = listToDoConverter.fromModel(listTodo);
+
+        Assertions.assertEquals(listTodoDTO.getClass(), result.getClass());
+    }
+
 }
